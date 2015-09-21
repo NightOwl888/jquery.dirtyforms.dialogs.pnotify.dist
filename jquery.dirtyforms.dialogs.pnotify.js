4a9e6a7627513e9b5cdefe1fa40f329b57241019
MyGet Build Services <noreply@myget.org>
2015-09-21 19:21:20 +0000

    Release version 2.0.0-beta00005

--- jquery.dirtyforms.dialogs.pnotify.js
+++ jquery.dirtyforms.dialogs.pnotify.js
@@ -1,25 +1,16 @@
 /*!
-PNotify dialog module (for jQuery Dirty Forms) | v2.0.0-beta00004 | github.com/snikch/jquery.dirtyforms
+PNotify dialog module (for jQuery Dirty Forms) | v2.0.0-beta00005 | github.com/snikch/jquery.dirtyforms
 (c) 2015 Shad Storhaug
 License MIT
 */
 
-// Support for UMD: https://github.com/umdjs/umd/blob/master/jqueryPluginCommonjs.js
-// This allows for tools such as Browserify to compose the components together into a single HTTP request.
-(function (factory) {
-    if (typeof define === 'function' && define.amd) {
-        // AMD. Register as an anonymous module.
-        define(['jquery'], factory);
-    } else if (typeof exports === 'object') {
-        // Node/CommonJS
-        module.exports = factory(require('jquery'), window, document);
-    } else {
-        // Browser globals
-        factory(jQuery, window, document);
-    }
-}(function ($, window, document, undefined) {
-    // Use ECMAScript 5's strict mode
-    "use strict";
+(function($, window, document, undefined) {
+    // Can't use ECMAScript 5's strict mode because several apps 
+    // including ASP.NET trace the stack via arguments.caller.callee 
+    // and Firefox dies if you try to trace through "use strict" call chains. 
+    // See jQuery issue (#13335)
+    // Support: Firefox 18+
+    //"use strict";
 
     var modal_overlay,
         notice,
@@ -134,4 +125,4 @@ License MIT
         selector: 'no-op'
     };
 
-}));
+})(jQuery, window, document);
